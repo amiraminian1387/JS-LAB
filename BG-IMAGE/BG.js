1b4a4a6891ec198c018d7a09dcacdaa1ebf30e08
Amir Aminian <amir.aminian1387@gmail.com>
2025-10-21 15:55:05 +0330

    version two of js is done usage of numbers

--- BG-IMAGE/BG.js
+++ BG-IMAGE/BG.js
@@ -1,15 +1,34 @@
 
 // ---(versian 1)---  ---usage of boolean az my teacher used---
 
-let flag = true;
+// let flag = true;
+
+// function changeBackground() {
+//     if (flag === true) {
+//         document.body.style.backgroundImage = "url('janati.jpg')";
+//         flag = false;
+//     } else  {
+//         document.body.style.backgroundImage = "url('kazem.jpg')";
+//         flag = true;
+//     }
+// }
+
+// ---(versian 2)---  ---usage of number = 0,1,2,3,0---
+let flag = 0;
 
 function changeBackground() {
-    if (flag === true) {
+    if (flag === 0) {
         document.body.style.backgroundImage = "url('janati.jpg')";
-        flag = false;
-    } else  {
+        flag = 1;
+    } else if (flag === 1) {
         document.body.style.backgroundImage = "url('kazem.jpg')";
-        flag = true;
+        flag = 2;
+    } else if (flag === 2) {
+        document.body.style.backgroundImage = "url('mohsen.jpg')";
+        flag = 3;
+    } else {
+        document.body.style.backgroundImage = "url('shamkhani.jpg')";
+        flag = 0;
     }
 }
 
