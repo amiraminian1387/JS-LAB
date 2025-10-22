8a22e477aedeed8133d779435c8aa641bcecc649
Amir Aminian <amir.aminian1387@gmail.com>
2025-10-22 23:30:13 +0330

    half way

--- SERVER-STATUS/SERVER.js
+++ SERVER-STATUS/SERVER.js
@@ -1,6 +1,9 @@
 function changeServerCondition() {
     let pass = 'SERVER IS RUNNING';
+    let fail = 'SERVER IS DOWN';
     const p = document.getElementById('condition');
     p.innerHTML = pass;
     p.style.color = 'green'; 
+    const btn = document.getElementById('btnCondition');
+    btn.style.display = 'none';
 }
