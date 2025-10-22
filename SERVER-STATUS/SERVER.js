9148b3e68358969091e38d4f509c2e87db0222d1
Amir Aminian <amir.aminian1387@gmail.com>
2025-10-22 19:19:58 +0330

    change p color and content with js

---
+++ SERVER-STATUS/SERVER.js
@@ -0,0 +1,6 @@
+function changeServerCondition() {
+    let pass = 'SERVER IS RUNNING';
+    const p = document.getElementById('condition');
+    p.innerHTML = pass;
+    p.style.color = 'green'; 
+}
