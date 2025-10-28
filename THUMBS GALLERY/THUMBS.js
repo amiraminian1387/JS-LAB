6eed2d5b67ce77dee9aee041060e9946145caf86
Amir Aminian <amir.aminian1387@gmail.com>
2025-10-28 20:31:19 +0330

    THUMBS GALLERY js is done

---
+++ THUMBS GALLERY/THUMBS.js
@@ -0,0 +1,19 @@
+function season1() {
+  const image = document.querySelector(".pictiure img");
+  image.setAttribute("src", "./images/1.webp");
+}
+
+function season2() {
+  const image = document.querySelector(".pictiure img");
+  image.setAttribute("src", "./images/2.webp");
+}
+
+function season3() {
+  const image = document.querySelector(".pictiure img");
+  image.setAttribute("src", "./images/3.webp");
+}
+
+function season4() {
+  const image = document.querySelector(".pictiure img");
+  image.setAttribute("src", "./images/4.webp");
+}
