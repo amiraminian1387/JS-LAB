0ed30f8db8475a449c8ce8871f06284c7d990559
Amir Aminian <amir.aminian1387@gmail.com>
2025-11-30 20:34:05 +0330

    REMEMBER GAME IS DONE

--- REMEMBER GAME/GAME.js
+++ REMEMBER GAME/GAME.js
@@ -1,19 +1,63 @@
 const OPEN_CARDS = [];
 const boxes = document.querySelectorAll("div");
+const h1 = document.querySelector("h1");
+const h2 = document.querySelector("h2");
+const startBtn = document.getElementById("start");
+
+
+
+let isLocked = false;
+let moves = 0;
+
+function showAllCardsTemporarily() {
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].classList.add("show");
+    }
+
+    setTimeout(() => {
+        for (let i = 0; i < boxes.length; i++) {
+            boxes[i].classList.remove("show");
+        }
+        isLocked = false; 
+    }, 3000); 
+}
+
+startBtn.addEventListener("click", () => {
+    moves = 0;
+    h1.textContent = "Moves: 0";
+    h2.textContent = "";
+    showAllCardsTemporarily();
+});
 
 
 function handleClick() {
+    if (isLocked) return;
+    if (OPEN_CARDS.length === 1 && OPEN_CARDS[0] === this) {
+        console.log("same card clicked");
+        return;
+    }
+
+
     OPEN_CARDS.push(this);
-    console.log(OPEN_CARDS)
-    this.classList.add("show")
+
+    this.classList.add("show");
     if (OPEN_CARDS.length === 2) {
+        moves++;
+        h1.textContent = "Moves: " + moves;
+    }
+    if (OPEN_CARDS.length === 2) {
+
         if (OPEN_CARDS[0].innerHTML === OPEN_CARDS[1].innerHTML) {
             OPEN_CARDS[0].classList.add("matched");
             OPEN_CARDS[1].classList.add("matched");
             OPEN_CARDS.length = 0;
+
+            checkWin();
         } else {
             OPEN_CARDS[0].classList.add("unmatched");
             OPEN_CARDS[1].classList.add("unmatched");
+
+            isLocked = true;
 
             setTimeout(function () {
                 OPEN_CARDS[0].classList.remove("show");
@@ -24,12 +68,30 @@ function handleClick() {
 
                 OPEN_CARDS.length = 0;
 
+                isLocked = false;
+
 
             }, 2000)
 
         }
     }
 }
+function checkWin() {
+    let allMatched = true;
+
+    for (let i = 0; i < boxes.length; i++) {
+        if (!boxes[i].classList.contains("matched")) {
+            allMatched = false;
+            break;
+        }
+    }
+
+    if (allMatched) {
+        h2.textContent = "congrats you are winner!!! 🎉" + moves + " moves!";
+    }
+}
+
+
 for (let i = 0; i < boxes.length; i++) {
     boxes[i].addEventListener('click', handleClick)
 }
